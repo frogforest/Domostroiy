5d3e587aa7c46b2cef576975d309dfc031487136
Великаныч <frogs.admirer@gmail.com>
2025-10-18 10:48:08 +0300

    коммит 3 в главной ветке!

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
     print(f"  - {math_task}")
     print(f"  - {finances_task}")
     print(f"  - {toys_task}")
-    print(f"Родитель молодец") #изменение для коммита
+    print(f"Делу - время, потехе - час")
 
     # Попытка взять задачи
     print("\n=== Попытка взять задачи ===")
